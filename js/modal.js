ec9dc7ea33690084fcd4c435d4a5d42d60170daa
Андрій <Emimo2396@gmail.com>
2022-08-14 19:05:24 +0300

    1

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,25 @@
-(() => {
+
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtnOrder: document.querySelector('[data-modal-order-open]'),
+    closeModalBtnOrder: document.querySelector('[data-modal-order-close]'),
+    openModalBtnCost: document.querySelector('[data-modal-cost-open]'),
+    closeModalBtnCost: document.querySelector('[data-modal-cost-close]'),
+    modalOrder: document.querySelector('[data-modal-order]'),
+    modalCost: document.querySelector('[data-modal-cost]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnOrder.addEventListener('click', toggleModalOrder);
+  refs.closeModalBtnOrder.addEventListener('click', toggleModalOrder);
+  refs.openModalBtnCost.addEventListener('click', toggleModalCost);
+  refs.closeModalBtnCost.addEventListener('click', toggleModalCost);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  
+
+  function toggleModalOrder() {
+    refs.modalOrder.classList.toggle('is-hidden');
+     refs.modalOrder.classList.toggle('modal-order');
   }
-})();
+  function toggleModalCost() {
+    refs.modalCost.classList.toggle('is-hidden');
+     refs.modalOrder.classList.toggle('modal-cost');
+  }
